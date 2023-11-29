e48c8fa912877ad071f37b13466215403b1570d1
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-29 19:45:19 +0530

    Added : initialized binary search tree

---
+++ src/com/bridgelabz/binarysearchtree/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.binarysearchtree;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!! welcome to binary search tree problem !!!");
+    }
+}
